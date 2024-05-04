351e175e376561e76db1342c6795c876cc32c84a
Oliver Sánchez <oliver-mbp@Olivers-MBP.attlocal.net>
2024-05-04 17:05:26 -0500

    wip; scraping portfolio_breakdown

--- get-coinbase-reports/main.py
+++ get-coinbase-reports/main.py
@@ -1,25 +1,18 @@
-import http.client
+import json
 import os
-
-from coinbase import jwt_generator
+from coinbase.rest import RESTClient
 
 key_name = os.environ.get("COINBASE_KEY_NAME")
 key_secret = os.environ.get("COINBASE_KEY_SECRET")
 
+def getPortfolioBreakdown():
+    client = RESTClient()
+    breakdown = client.get_portfolio_breakdown("fe02815f-af68-536c-89f2-74613329224e")
+    return breakdown
 
-def createReport():
-    conn = http.client.HTTPSConnection("api.exchange.coinbase.com")
-    payload = ''
-    headers = {
-        'Content-Type': 'application/json',
-        'cb-access-timestamp': time.time()
-    }
-    conn.request("POST", "/reports", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
 def main():
-    createReport()
+    print(getPortfolioBreakdown())
+
 
 if __name__ == '__main__':
     main()
