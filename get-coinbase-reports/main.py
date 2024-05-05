96e94258be38de2db3b61f53ca3b7adb821095bb
Oliver Sánchez <oliver-mbp@Olivers-MBP.attlocal.net>
2024-05-04 22:08:52 -0500

    Added multiple recipient support, parameterized sender

--- get-coinbase-reports/main.py
+++ get-coinbase-reports/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from json2html import *
 from coinbase.rest import RESTClient
@@ -7,7 +8,8 @@ from botocore.exceptions import ClientError
 key_name = os.environ.get("COINBASE_KEY_NAME")
 key_secret = os.environ.get("COINBASE_KEY_SECRET")
 portfolio_uuid = os.environ.get("PORTFOLIO_UUID")
-recipient_email_list = os.environ.get("RECIPIENT_EMAIL_LIST")
+recipient_email_list = json.loads(os.environ.get("RECIPIENT_EMAIL_LIST"))
+sender_email_address = os.environ.get("SENDER_EMAIL_ADDRESS")
 
 
 def getPortfolioBreakdown():
@@ -40,9 +42,9 @@ def emailReport(report_html):
 
     try:
         response = ses_client.send_email(
-            Source='trickquestionis@gmail.com',
+            Source=sender_email_address,
             Destination={
-                'ToAddresses': [recipient_email_list]
+                'ToAddresses': recipient_email_list
             },
             Message=message
         )
